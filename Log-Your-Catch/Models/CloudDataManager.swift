501f8335e326e9043866ec95f59bbd2cbe81a7be
liam-strand <strandliam@gmail.com>
2020-08-09 17:53:03 -0400

    Recolor icons replace color sets

--- Log-Your-Catch/Models/CloudDataManager.swift
+++ Log-Your-Catch/Models/CloudDataManager.swift
@@ -25,7 +25,7 @@ class CloudDataManager {
             if attemptToUploadCasesDebug { print("already logged in") }
             performUpload()
             clearLocalData(array, saveFunc)
-            completion(false)
+            completion(true)
             
         } else {
             let user = defaults.string(forKey: "User") ?? ""
@@ -42,7 +42,7 @@ class CloudDataManager {
                         if attemptToUploadCasesDebug { print("logged in from local data") }
                         self.performUpload()
                         self.clearLocalData(array, saveFunc)
-                        completion(false)
+                        completion(true)
                     } else {
                         if attemptToUploadCasesDebug { print("error logging in") }
                         completion(true)
